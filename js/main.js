90c7c709da96edd8ccecf11aa37ad77ff6b8e2e3
Heetarthi Kamdar <heetarthikamdar@gmail.com>
2019-05-14 11:32:27 +0530

    updates

--- js/main.js
+++ js/main.js
@@ -69,3 +69,70 @@ $('.toggle-legacy-view').on('click', function (event) {
   $('html')
     .toggleClass('csstransforms generatedcontent no-csstransforms no-generatedcontent');
 });
+
+
+
+
+/*global $, document, setTimeout, i*/
+$(document).ready(function () {
+
+  'use strict';
+
+  var slide  = $('.slide'),
+      slideAelements = $('.slide-a-child'),
+      slideBelements = $('.slide-b-child'),
+      slideDelements = $('.slide-d-child'),
+      replay = $('button.replay');
+
+
+/*Stopping it figure here out */
+  slide.each(function (i) {
+      if (i < 2) {
+          setTimeout(function () {
+              slide.eq(i).fadeOut();
+          }, 5000 * (i + 1));
+      }
+  });
+
+
+  function animateSlideA() {
+    slideAelements.each(function (i) {
+        setTimeout(function () {
+            slideAelements.eq(i).addClass('is-visible');
+        }, 300 * (i + 1));
+    });
+  }
+
+  function animateSlideB() {
+    slideBelements.each(function (i) {
+        setTimeout(function () {
+            slideBelements.eq(i).addClass('is-visible');
+        }, 300 * (i + 1));
+    });
+ }
+
+  function animateSlideD() {
+    slideDelements.each(function (i) {
+        setTimeout(function () {
+            slideDelements.eq(i).addClass('is-visible');
+        }, 300 * (i + 1));
+    });
+  }
+
+  animateSlideA();
+
+  setTimeout(function () {
+     animateSlideB();
+  }, 5000);
+
+  setTimeout(function () {
+     animateSlideD();
+  }, 10000);
+
+
+
+ replay.on('click', function () {
+   location.reload(true);
+ });
+
+});
